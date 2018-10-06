234e9fb8b79ffa7b8b9d7c9be5a8b421c0b96250
Apostolis Planas <apostolisplanas@gmail.com>
2018-10-06 12:15:18 +0200

    Project JoinAlgorithmsOverCassandraDBs

---
+++ src/CassandraJoins/CassandraJoinsException.java
@@ -0,0 +1,21 @@
+package CassandraJoins;
+
+/**
+ * Exception class for CassandraJoins JAVA API 
+ * 
+ * @author Fountouris Antonios
+ * @author Planas Apostolos
+ */
+public class CassandraJoinsException extends Exception
+{
+private static final long serialVersionUID = 7526472295622776147L;
+
+      public CassandraJoinsException() {}
+
+      public CassandraJoinsException(String message)
+      {
+         super(message);
+      }
+ }
+
+
